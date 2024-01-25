c6f0ecfa630dc0b88d4a2c6e886dea9ae18c10e7
biyash <bsstha21@gmail.com>
2024-01-25 18:42:36 +0545

    learned props in react js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
 import Objects from './Objects';  
-import Practice3 from './Practice3'; // Corrected import statement
-import Practice4 from './Practice4';
-import Datafetching from './Datafetching';
-import Maps from './Maps';
-import Tailblocks from './Tailblocks';
+
 function App() {
   return (
     <div>
      
-
-      <Datafetching /> 
-     
+   <Objects title="title1" />   
+   <Objects title="title2" />   
+   <Objects title="title3" />   
     </div>
   );
 }
