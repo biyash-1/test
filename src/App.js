89df543be79c3501cf6bc2c637151e8a6fc4d1a3
biyash <bsstha21@gmail.com>
2024-01-29 18:36:42 +0545

    learned fetching data using axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import Objects from './Objects';  
+import DataFetching from './Datafetching';
 
 function App() {
   return (
     <div>
      
-   <Objects title="title1" />   
+   {/* <Objects title="title1" />   
    <Objects title="title2" />   
-   <Objects title="title3" />   
+   <Objects title="title3" />    */}
+
+   <DataFetching />
     </div>
   );
 }
