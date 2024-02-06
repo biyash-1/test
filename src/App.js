31ae9b4b779bb9939c4869113a30597c1b1a7964
biyash <bsstha21@gmail.com>
2024-02-06 08:15:55 +0545

    Update file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
-import Objects from './Objects';  
-import DataFetching from './Datafetching';
+import React, { useState } from "react";
+import Objects from "./Objects";
+import DataFetching from "./Datafetching";
 
 function App() {
   return (
     <div>
-     
-   {/* <Objects title="title1" />   
+      {/* <Objects title="title1" />   
    <Objects title="title2" />   
    <Objects title="title3" />    */}
 
-   <DataFetching />
+      <DataFetching />
     </div>
   );
 }
 
 export default App;
-
-
